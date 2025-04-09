413f39007cc1a889fbe3a6a8377fbd36f02aeb67
Harsh <hb755037@gmail.com>
2025-04-09 09:14:38 +0530

    created connection

--- data_source.py
+++ data_source.py
@@ -1,6 +1,29 @@
 import time
+# data_source.py
+import pyodbc
 
 def fetch_data():
-    # Simulate a slow DB/API response
-    time.sleep(2)
-    return {"message": "This is data from the database!"}
+    
+    conn_str = (
+        "DRIVER={SQL Server};"
+        "SERVER=LAPTOP-S8HL2J8D\\SQLEXPRESS;"
+        "DATABASE=webCachingserver;"
+        "Trusted_Connection=yes;"
+    )
+
+    try:
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        print("Connected successfully!")
+        time.sleep(2)
+        cursor.execute("SELECT * FROM emp")
+        data = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
+        
+        return {"message": "Connected successfully!", "rows": data}
+
+    except Exception as e:
+        print("Connection failed:", e)
+        return {"error": str(e)}
+
+
+
